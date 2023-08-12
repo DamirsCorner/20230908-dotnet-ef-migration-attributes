5ef2c8e3a638f1ab48f0c8c20727b27b02b2643e
Damir Arh <damir.arh@gmail.com>
2023-08-12 11:37:27 +0200

    Add project files.

---
+++ EfCoreMigrationAttributes/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace EfCoreMigrationAttributes;
+public class Person
+{
+    public int PersonId { get; set; }
+    public string FirstName { get; set; }
+    public string? MiddleName { get; set; }
+    public string LastName { get; set; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+}
---
+++ EfCoreMigrationAttributes/PersonDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfCoreMigrationAttributes;
+public class PersonDbContext : DbContext
+{
+    public PersonDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Person> Persons => Set<Person>();
+}
